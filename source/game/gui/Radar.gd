c8861958bc3a4724224cb83118be9fb7bf9e43f8
Lancelot de Ferrière <wraitii@gmail.com>
2019-12-14 22:40:58 +0100

    Functional-ish 3D radar, letting me dogfight somewhat

--- source/game/gui/Radar.gd
+++ source/game/gui/Radar.gd
@@ -1,54 +1,49 @@
 extends Control
 
-const JumpZone = preload('../JumpZone.gd')
-var radar = {}
+var scale = 50
 
-func _create_pixel(size, color):
-	var pixel = Polygon2D.new()
-	pixel.polygon = [
-		Vector2(-0.5,-0.5),
-		Vector2(0.5,-0.5),
-		Vector2(0.5,0.5),
-		Vector2(-0.5,0.5)
-	]
-	pixel.scale = Vector2(size, size)
-	pixel.color = color
-	return pixel
+var items = {}
 
 func _process(delta):
 	var playerShip = Core.gameState.playerShip
 	if !playerShip:
-		return;
-	
-	## TODO: Handle rotations of the ship in Follow mode
-	
-	var velocity = playerShip.get_linear_velocity()
-	velocity = Vector2(velocity.x, -velocity.z);
-	var angle = velocity.normalized().angle_to(Vector2(1,0))
-	get_node('center/movementArrow').rotation = angle
-	var scaleR = playerShip.get_linear_velocity().length() / playerShip.stat('max_speed');
-	get_node('center/movementArrow').scale = Vector2(scaleR, scaleR)
-	
-	var items = get_tree().get_nodes_in_group('Landables')
-	for l in items:
-		if !(l.name in radar):
-			radar[l.name] = _create_pixel(5, Color(1,1,0))
-			get_node('center').add_child(radar[l.name])
-		var p = (l.position - playerShip.translation)
-		radar[l.name].position = Vector2(p.x, p.z) * 0.005
+		return
 
-	items = get_tree().get_nodes_in_group('JumpZones')
-	for l in items:
-		if !(l.name in radar):
-			radar[l.name] = _create_pixel(5, Color(1,0,1))
-			get_node('center').add_child(radar[l.name])
-		var p = (l.translation - playerShip.translation)
-		radar[l.name].position = Vector2(p.x, p.z) * 0.005
+	var f = playerShip.transform.xform(Vector3(0, 1*scale, 10*scale))
+	var u = playerShip.transform.xform(Vector3(0, 1*scale + 1, 10*scale)) - f
+	$VPC/VP/Camera.look_at_from_position(f/scale, playerShip.translation/scale, u)
+	
+	$VPC/VP/XZ.translation = playerShip.translation/scale
+	$VPC/VP/XZ.rotation = playerShip.rotation
 
-	items = get_tree().get_nodes_in_group('sector_ships')
-	for l in items:
-		if !(l.name in radar):
-			radar[l.name] = _create_pixel(2, Color(0,1,1))
-			get_node('center').add_child(radar[l.name])
-		var p = (l.translation - playerShip.translation)
-		radar[l.name].position = Vector2(p.x, p.z) * 0.005
+	for ship in get_tree().get_nodes_in_group('sector_ships'):
+		if ship == playerShip:
+			continue
+		if !(ship.ID in items):
+			items[ship.ID] = $VPC/VP/Ship.duplicate()
+			items[ship.ID].visible = true
+			$VPC/VP.add_child(items[ship.ID])
+			ship.connect('tree_exiting', self, "_on_exit", [ship.ID])
+		
+		var p = Plane(u, 0)
+		var o = u
+		var pt = p.intersects_ray(ship.translation/scale - playerShip.translation/scale, -u)
+		if !pt:
+			o = -u
+			pt = p.intersects_ray(ship.translation/scale - playerShip.translation/scale, u)
+	
+		items[ship.ID].get_node('dot').translation = ship.translation/scale
+		if pt:
+			items[ship.ID].get_node('point').show()
+			items[ship.ID].get_node('point').translation = pt + playerShip.translation/scale
+			items[ship.ID].get_node('point').look_at(pt + o + playerShip.translation/scale, Vector3(0,1,0))
+			var d = (ship.translation/scale - pt - playerShip.translation/scale).length()
+			print(d)
+			items[ship.ID].get_node('point').get_node('point').set_scale(Vector3(1,1, d))
+			items[ship.ID].get_node('point').get_node('point').translation = Vector3(0,0,-d/2)
+		else:
+			items[ship.ID].get_node('point').hide()
+
+func _on_exit(ID):
+	$VPC/VP.remove_child(items[ID])
+	items.erase(ID)
